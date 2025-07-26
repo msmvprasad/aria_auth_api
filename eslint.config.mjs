95d26ce3a457f8733f265ad7a69a734546de977c
Prasad Mirthipati <msmvprasad@gmail.com>
2025-07-26 22:15:16 +0530

    Update ESLint to flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['node_modules/**', 'dist/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...tsPlugin.configs['recommended-type-checked'].rules,
+    },
+  },
+  prettier,
+];
